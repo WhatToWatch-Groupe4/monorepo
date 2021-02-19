463c7e7ca67891bd0d5fc65e2a23d4f842f16ee6
Romain Fréchet <romainfpro@gmail.com>
2021-02-19 09:46:50 +0100

    feat: clean with yarn lint

--- apps/web/src/components/WishList.tsx
+++ apps/web/src/components/WishList.tsx
@@ -1,12 +1,12 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-function WishList(){
-    const id = 664767;
+function WishList() {
+  const id = 664767;
 
-    const user_id = "22C32";
+  const user_id = '22C32';
 
-    useEffect(() => {
-        const res = fetch(`http://localhost:3000/wishlist`, {method: 'POST'})
-    },[]);
+  useEffect(() => {
+    const res = fetch(`http://localhost:3000/wishlist`, { method: 'POST' });
+  }, []);
 }
 export default WishList();
