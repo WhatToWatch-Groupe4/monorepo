494c736f0312cad8d9cdc6cbcd6901f34ee9a6a6
David A <d.ananda974@gmail.com>
2021-02-19 14:04:01 +0100

    feat(web): clickable movie card to /movies/id

--- apps/web/src/components/MovieList.tsx
+++ apps/web/src/components/MovieList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { MovieResponse } from 'moviedb-promise/dist/request-types';
+import { Link } from 'react-router-dom';
 
 function MovieList() {
   const [movies, setMovie] = useState<Array<MovieResponse> | null>([]);
@@ -17,21 +18,23 @@ function MovieList() {
       <h1 className="text-4xl text-white uppercase font-bold mb-4 pt-3">TOUS LES FILMS / SÉRIES</h1>
       <div className="grid md:grid-flow-col grid-cols-6 grid-rows-3 gap-0">
         {movies.map((movie) => (
-          <div className="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-              className="bg-gray-300 h-64 rounded-lg shadow-md bg-cover bg-center"
-            />
-            <div className="py-2 text-center text-white font-bold uppercase tracking-wide">{movie.title}</div>
-            <div className="flex items-center text-white justify-between py-2 px-3 ">
-              <p className="text-white">
-                {JSON.stringify(movie.vote_average)}/10 ⭐{/*{movie.genres?.map((genre) => (*/}
-                {/*  <p>{genre}</p>*/}
-                {/*))}*/}
-              </p>
+          <Link to={'/movies/' + movie.id}>
+            <div className="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+                className="bg-gray-300 h-64 rounded-lg shadow-md bg-cover bg-center"
+              />
+              <div className="py-2 text-center text-white font-bold uppercase tracking-wide">{movie.title}</div>
+              <div className="flex items-center text-white justify-between py-2 px-3 ">
+                <p className="text-white">
+                  {JSON.stringify(movie.vote_average)}/10 ⭐{/*{movie.genres?.map((genre) => (*/}
+                  {/*  <p>{genre}</p>*/}
+                  {/*))}*/}
+                </p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
