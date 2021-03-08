ac1daa062f1cdc6a7664b10716cbea43081d8f73
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2021-03-08 14:38:27 +0100

    build(all): fix to make linter happy

--- apps/web/src/components/SideMenu.tsx
+++ apps/web/src/components/SideMenu.tsx
@@ -12,55 +12,38 @@ const SideMenu: FunctionComponent = () => {
   if (!initialized) {
     return null;
   }
-  if (keycloak.authenticated) {
-    return (
-      <div className="w-32 bg-black-20 h-screen fixed">
+
+  return (
+    <div className="w-32 bg-black-20 h-screen fixed">
+      <Link to={''}>
+        <img src={logo} alt="logo" className="p-6" />
+      </Link>
+      <div className="h-0.5 bg-white opacity-40 w-16 m-auto my-4" />
+      <div className="menu">
         <Link to={''}>
-          <img src={logo} alt="logo" className="p-6" />
+          <img
+            src={clapperboard}
+            alt="logo"
+            className="m-auto px-11 py-6 opacity-50 hover:opacity-100 cursor-pointer"
+          />
         </Link>
-        <div className="h-0.5 bg-white opacity-40 w-16 m-auto my-4" />
-        <div className="menu">
-          <Link to={''}>
-            <img
-              src={clapperboard}
-              alt="logo"
-              className="m-auto px-11 py-6 opacity-50 hover:opacity-100 cursor-pointer"
-            />
-          </Link>
-          <Link to={'/wishlist'}>
-            <img src={clock} alt="logo" className="m-auto px-10 py-6 opacity-50 hover:opacity-100 cursor-pointer" />
-          </Link>
-          <Link to={'/viewlist'}>
-            <img
-              src={binoculars}
-              alt="logo"
-              className="m-auto px-11 py-6 opacity-50 hover:opacity-100 cursor-pointer"
-            />
-          </Link>
-        </div>
+        {keycloak.authenticated && (
+          <>
+            <Link to={'/wishlist'}>
+              <img src={clock} alt="logo" className="m-auto px-10 py-6 opacity-50 hover:opacity-100 cursor-pointer" />
+            </Link>
+            <Link to={'/viewlist'}>
+              <img
+                src={binoculars}
+                alt="logo"
+                className="m-auto px-11 py-6 opacity-50 hover:opacity-100 cursor-pointer"
+              />
+            </Link>
+          </>
+        )}
       </div>
-    );
-  } else {
-    return (
-      <div className="w-32 bg-black-20 h-screen fixed">
-        <Link to={''}>
-          <img src={logo} alt="logo" className="p-6" />
-        </Link>
-        <div className="h-0.5 bg-white opacity-40 w-16 m-auto my-4" />
-        <div className="menu">
-          <Link to={''}>
-            <img
-              src={clapperboard}
-              alt="logo"
-              className="m-auto px-11 py-6 opacity-50 hover:opacity-100 cursor-pointer"
-            />
-          </Link>
-          <img src={clock} alt="logo" className="m-auto px-11 py-6 opacity-50 hover:opacity-100 cursor-pointer" />
-          <img src={binoculars} alt="logo" className="m-auto px-11 py-6 opacity-50 hover:opacity-100 cursor-pointer" />
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default SideMenu;
