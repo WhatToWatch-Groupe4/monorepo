0846e18a13030566ba1eb2237e411e48cc670aa7
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2021-03-08 11:59:11 +0100

    style(api): make linter happy

--- apps/api/src/comments/comments.service.ts
+++ apps/api/src/comments/comments.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotImplementedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCommentDto } from './dto/create-comment.dto';
@@ -9,23 +9,23 @@ import { Comment } from './entities/comment.entity';
 export class CommentsService {
   constructor(@InjectRepository(Comment) private readonly commentRepository: Repository<Comment>) {}
 
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  create(createCommentDto: CreateCommentDto): Comment {
+    throw new NotImplementedException({ createCommentDto });
   }
 
   findAll(movieId: number): Promise<Comment[]> {
     return this.commentRepository.find({ movieId });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
+  findOne(id: number): Comment {
+    throw new NotImplementedException({ id });
   }
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
+  update(id: number, updateCommentDto: UpdateCommentDto): Comment {
+    throw new NotImplementedException({ id, updateCommentDto });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+  remove(id: number): void {
+    throw new NotImplementedException({ id });
   }
 }
