fe883f347d26f77f356c797a5ff0828775e057f0
Romain Fréchet <romainfpro@gmail.com>
2021-03-09 13:01:37 +0100

    feat(test): remove unused import

--- apps/api/test/movie.e2e-spec.ts
+++ apps/api/test/movie.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { getConnection } from 'typeorm';
 
 describe('MovieController (e2e)', () => {
   let app: INestApplication;
