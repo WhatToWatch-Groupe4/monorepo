4260d4d0cabd6b1ecb44b3c95c505b94554baf99
Romain Fréchet <romainfpro@gmail.com>
2021-03-09 12:11:10 +0100

    feat(test): add providers to make the test work properly

--- apps/api/src/comments/comments.service.spec.ts
+++ apps/api/src/comments/comments.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { CommentsService } from './comments.service';
+import { Comment } from './entities/comment.entity';
 
 describe('CommentsService', () => {
   let service: CommentsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CommentsService],
+      providers: [
+        CommentsService,
+        {
+          provide: getRepositoryToken(Comment),
+          useValue: 'commentRepository',
+        },],
     }).compile();
 
     service = module.get<CommentsService>(CommentsService);
--- apps/api/src/views/views.controller.spec.ts
+++ apps/api/src/views/views.controller.spec.ts
@@ -1,11 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { ViewsController } from './views.controller';
+import { View } from './views.entity';
+import { ViewsService } from './views.service';
 
 describe('ViewsController', () => {
   let controller: ViewsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ViewsService,
+        {
+          provide: getRepositoryToken(View),
+          useValue: 'viewRepository',
+        },
+      ],
       controllers: [ViewsController],
     }).compile();
 
