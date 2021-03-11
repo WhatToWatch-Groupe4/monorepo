aab067c23d705e7daa9b2a11ec03943551ad79bf
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2021-03-11 11:30:14 +0100

    test(web): fix tests

--- apps/web/cypress/integration/movie.spec.ts
+++ apps/web/cypress/integration/movie.spec.ts
@@ -2,15 +2,15 @@
 
 export default context('Movie', () => {
   beforeEach(() => {
-    cy.intercept('http://localhost:3000/movies/*', { fixture: 'movie.json' });
-    cy.intercept('http://localhost:3000/movies/', { fixture: 'movies.json' });
-    cy.intercept('http://localhost:3000/comments', { fixture: 'comments.json' });
-    cy.intercept('get', 'http://localhost:3000/views', { id: 1 });
-    cy.intercept('post', 'http://localhost:3000/views', { id: 1 });
-    cy.intercept('delete', 'http://localhost:3000/views', { id: 1 });
-    cy.intercept('get', 'http://localhost:3000/wishlist', { userUuid: 1 });
-    cy.intercept('post', 'http://localhost:3000/wishlist', { userUuid: 1 });
-    cy.intercept('delete', 'http://localhost:3000/wishlist', { userUuid: 1 });
+    cy.intercept('movies/*', { fixture: 'movie.json' });
+    cy.intercept('movies/', { fixture: 'movies.json' });
+    cy.intercept('comments', { fixture: 'comments.json' });
+    cy.intercept('get', 'views', { id: 1 });
+    cy.intercept('post', 'views', { id: 1 });
+    cy.intercept('delete', 'views', { id: 1 });
+    cy.intercept('get', 'wishlist', { userUuid: 1 });
+    cy.intercept('post', 'wishlist', { userUuid: 1 });
+    cy.intercept('delete', 'wishlist', { userUuid: 1 });
     cy.visit('http://localhost:3001/');
     cy.get('#movie-list a:first').click();
   });
@@ -22,7 +22,7 @@ export default context('Movie', () => {
     cy.get('.btn-movie-not-view').should('be.disabled');
   });
 
-  it('Log in', () => {
+  it('Check actions when user is logged in', () => {
     const username = Cypress.env('username');
     const password = Cypress.env('password');
 
@@ -30,9 +30,11 @@ export default context('Movie', () => {
     cy.get('#username').type(username).should('have.value', username);
     cy.get('#password').type(password).should('have.value', password);
     cy.get('#kc-login').click();
-  });
 
-  it('Check actions when user is logged in', () => {
+    cy.visit('http://localhost:3001/');
+    cy.wait(1000);
+    cy.get('#movie-list a:first').click();
+
     cy.location('pathname').should('contain', '/movies');
 
     // Viewlist btn
